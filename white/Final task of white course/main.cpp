6646ac22ee021a4a6d8aac1e2fcdcb6112ee2e0f
davyzlarc <vvlaad@mail.ru>
2021-07-06 01:13:41 +0300

    Created template of main.cpp in final task

---
+++ white/Final task of white course/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Date {
+public:
+  int GetYear() const;
+  int GetMonth() const;
+  int GetDay() const;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+  void AddEvent(const Date& date, const string& event);
+  bool DeleteEvent(const Date& date, const string& event);
+  int  DeleteDate(const Date& date);
+
+  /* type (not void) */ void Find(const Date& date) const;
+  
+  void Print() const;
+};
+
+int main() {
+  Database db;
+    
+  string command;
+  while (getline(cin, command)) {
+    // ...
+  }
+
+  return 0;
+}
