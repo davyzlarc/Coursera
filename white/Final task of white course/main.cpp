acec388df5ed4ce0a9ad5ea0cf54c4af9cc8ae27
davyzlarc <vvlaad@mail.ru>
2021-07-07 23:41:41 +0300

    Added constructor into the class Data

--- white/Final task of white course/main.cpp
+++ white/Final task of white course/main.cpp
@@ -1,34 +1,73 @@
 #include <iostream>
+#include <set>
+#include <map>
+#include <sstream>
 
 using namespace std;
 
-class Date {
+class Date
+{
 public:
-  int GetYear() const;
-  int GetMonth() const;
-  int GetDay() const;
+    Date(const string &s)
+    {
+        stringstream ss;
+        ss << s;
+        char ch;
+        if (!(ss >> Year and ss >> ch and ch == '-' and ss >> Month and ss >> ch and ch == '-' and ss >> Day))
+            throw invalid_argument("Wrong date format: " + s);
+        else
+        {
+            if (!(Month >= 0 and Month <= 12))
+                throw invalid_argument("Month value is invalid: " + to_string(Month));
+            else if (!(Day >= 1 and Day <= 31))
+                throw invalid_argument("Day value is invalid: " + to_string(Day));
+        }
+    }
+
+    int GetYear() const;
+    int GetMonth() const;
+    int GetDay() const;
+
+private:
+    int Day;
+    int Month;
+    int Year;
 };
 
-bool operator<(const Date& lhs, const Date& rhs);
+bool operator<(const Date &lhs, const Date &rhs);
 
-class Database {
+class Database
+{
 public:
-  void AddEvent(const Date& date, const string& event);
-  bool DeleteEvent(const Date& date, const string& event);
-  int  DeleteDate(const Date& date);
+    void AddEvent(const Date &date, const string &event);
+    bool DeleteEvent(const Date &date, const string &event);
+    int DeleteDate(const Date &date);
 
-  /* type (not void) */ void Find(const Date& date) const;
-  
-  void Print() const;
+    /* type (not void) */ void Find(const Date &date) const;
+
+    void Print() const;
+
+private:
+    map<Date, set<string>> tuple;
 };
 
-int main() {
-  Database db;
-    
-  string command;
-  while (getline(cin, command)) {
-    // ...
-  }
+int main()
+{
+    Database db;
+   
+    try{
+    Date d("");
+    }
+    catch(exception &ex)
+    {
+        cout << ex.what();
+    }
 
-  return 0;
-}
+    string command;
+    while (getline(cin, command))
+    {
+        // ...
+    }
+
+    return 0;
+} 
